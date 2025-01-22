7047ad315ee25ffee6958ff534a8e0e178383cc9
ZSedillo <sedillozandro720@gmail.com>
2025-01-22 22:00:28 +0800

    First output

--- teamweb/src/App.jsx
+++ teamweb/src/App.jsx
@@ -1,13 +1,15 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [items,setItems] = useState([])
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await fetch('http://localhost:3000'); // Correct the URL if needed
-        const data = await res.json(); // Call the .json() method and await its result
-        console.log(data); // Logs the actual JSON data
+        const res = await fetch('http://localhost:3000');
+        const data = await res.json();
+        //console.log(data);//Show in Console [Debugging Purposes]
+        setItems(data.items)
       } catch (error) {
         console.error('Error fetching data:', error);
       }
@@ -17,6 +19,9 @@ function App() {
 
   return (
     <>
+    {items.map(i =>(
+      <p>{i.name},{i.description}</p>
+    ))}
     </>
   );
 }
