ac23ae0b2e9886cb9228cb2ea7a33f57c2a79435
oliskiee-dev <oli.deniel.garcia@gmail.com>
2025-02-05 23:50:09 +0800

    added header and footer

--- teamweb/src/Viewer/PreRegistration/Pre-registration.jsx
+++ teamweb/src/Viewer/PreRegistration/Pre-registration.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import Header from '../Component/Header.jsx';
+import Footer from '../Component/Footer.jsx';
 
 function PreRegistration(){
     return(
         <>
+        <Header/>
         {/* Input Here */}
         <h1>MARC GOCON</h1>
+        <Footer/>
         </>
     )
 }
