f12ff5312042586fd3893a91a3cef404e540737e
ZSedillo <sedillozandro720@gmail.com>
2025-02-07 16:49:31 +0800

    Testing Announcement Schema

--- server/models/Annoucement.js
+++ server/models/Annoucement.js
@@ -4,7 +4,10 @@ const announcementSchema = new mongoose.Schema({
     title: String,
     description: String,
     img_url: String,
-    created_at: String,
+    created_at: {
+        type: Date, // Change this from String to Date
+        default: Date.now, // Optional: Set a default value as the current date
+    },
 });
 
 const announcementModel = mongoose.model("Announcement", announcementSchema);
