10db472e0093d61c04ecb58311baf8fc31dc2739
ZSedillo <sedillozandro720@gmail.com>
2025-02-07 17:29:43 +0800

    Test

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ app.get('/test', async (req,res) =>{
 //Get all Announcements
 app.get('/announcement', async (req,res) =>{
     const response = await itemModel.find();
-    return res.json({announcements : response});
+    return res.json({items : response});
 })
 
 
