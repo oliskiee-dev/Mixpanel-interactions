191af73e20964339503af21d6eff02a71b2963cf
ZSedillo <sedillozandro720@gmail.com>
2025-02-28 15:42:04 +0800

    Fixing Homepage schema

--- server/models/Homepage.js
+++ server/models/Homepage.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
 
 const homepageSchema = new mongoose.Schema({
-    image_url:String,
-    created_at: Date,
-})
+    image_url: String,
+    created_at: { type: Date, default: Date.now },
+});
 
 
 const homepageModel = mongoose.model("homepage",homepageSchema)
