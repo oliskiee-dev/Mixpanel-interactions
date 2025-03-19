3c64d07e281878f0e38b700628d75a942ec97a29
ZSedillo <sedillozandro720@gmail.com>
2025-03-19 23:43:21 +0800

    Adding register

--- server/index.js
+++ server/index.js
@@ -238,24 +238,24 @@ app.get('/admin-homepage', authenticate, (req, res) => {
 
 
 //REGISTER TEMP CODE
-// app.post('/register', async (req, res) => {
-//     const { username, password } = req.body;
+app.post('/register', async (req, res) => {
+    const { username, password } = req.body;
 
-//     try {
-//         const existingUser = await userModel.findOne({ username });
-//         if (existingUser) return res.status(400).json({ error: "Username already exists" });
+    try {
+        const existingUser = await userModel.findOne({ username });
+        if (existingUser) return res.status(400).json({ error: "Username already exists" });
 
-//         const salt = await bcrypt.genSalt(10);
-//         const hashedPassword = await bcrypt.hash(password, salt);
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
 
-//         const newUser = new userModel({ username, password: hashedPassword });
-//         await newUser.save();
+        const newUser = new userModel({ username, password: hashedPassword });
+        await newUser.save();
 
-//         res.json({ message: "User registered successfully" });
-//     } catch (error) {
-//         res.status(500).json({ error: "Server error" });
-//     }
-// });
+        res.json({ message: "User registered successfully" });
+    } catch (error) {
+        res.status(500).json({ error: "Server error" });
+    }
+});
 
 // app.use("/announcement", router);
 // app.use("/announcement", express.static(path.join(__dirname, "announcement")));
