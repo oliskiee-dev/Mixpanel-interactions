94c127602859894b52e8484f47e5f7fdc341e8ef
ZSedillo <sedillozandro720@gmail.com>
2025-03-22 21:55:51 +0800

    Updating book schema

--- server/models/Book.js
+++ server/models/Book.js
@@ -2,16 +2,17 @@ const mongoose = require('mongoose');
 
 const bookSchema = new mongoose.Schema({
   availability: {
-    Monday: [{ start: String, end: String }],
-    Tuesday: [{ start: String, end: String }],
-    Wednesday: [{ start: String, end: String }],
-    Thursday: [{ start: String, end: String }],
-    Friday: [{ start: String, end: String }],
-    Saturday: [{ start: String, end: String }],
-    Sunday: [{ start: String, end: String }],
+    Monday: [String],
+    Tuesday: [String],
+    Wednesday: [String],
+    Thursday: [String],
+    Friday: [String],
+    Saturday: [String],
+    Sunday: [String],
   },
 });
 
 const bookModel = mongoose.model("book", bookSchema);
 
 module.exports = bookModel;
+
