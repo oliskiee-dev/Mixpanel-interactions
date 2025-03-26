3cb5708531f7899267ec402432d2ac08ad590bf9
ZSedillo <sedillozandro720@gmail.com>
2025-03-26 21:50:23 +0800

    Added roles for admin

--- server/models/User.js
+++ server/models/User.js
@@ -19,6 +19,22 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true
   },
+  role: {
+    type: String,
+    enum: ['head_admin', 'admin'], // Roles: One head_admin, multiple admins, default user
+    default: 'admin'
+  }
+});
+
+// Ensure only one Head Admin exists
+userSchema.pre('save', async function (next) {
+  if (this.role === 'head_admin') {
+    const existingHeadAdmin = await mongoose.model('User').findOne({ role: 'head_admin' });
+    if (existingHeadAdmin) {
+      throw new Error('There can only be one Head Admin.');
+    }
+  }
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
