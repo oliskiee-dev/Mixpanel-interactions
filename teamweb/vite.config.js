966deca38198b035290b21da0d2e593167ee9193
ZSedillo <sedillozandro720@gmail.com>
2025-03-29 01:15:42 +0800

    changes in vite.config.js

--- teamweb/vite.config.js
+++ teamweb/vite.config.js
@@ -1,7 +1,22 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Allows Railway to access the server
+    port: process.env.PORT || 4173, // Uses Railway's assigned port or defaults to 4173
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 4173,
+  }
 })
