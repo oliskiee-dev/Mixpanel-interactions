be07cbde2b831132a6ecedce242800409070e21b
ZSedillo <sedillozandro720@gmail.com>
2025-04-03 23:07:04 +0800

    Fixing region in s3-credentials

--- server/util/s3-credentials.js
+++ server/util/s3-credentials.js
@@ -2,6 +2,8 @@ const { S3Client } = require("@aws-sdk/client-s3");
 
 exports.s3Client = new S3Client({
     region: process.env.AWS_REGION,
+    endpoint: `https://s3.${process.env.AWS_REGION}.amazonaws.com`, // Explicit endpoint
+    forcePathStyle: true, // Ensures proper URL structure
     credentials: {
         accessKeyId: process.env.AWS_ACCESS_KEY,
         secretAccessKey: process.env.AWS_SECRET_KEY,
