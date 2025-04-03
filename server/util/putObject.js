176436bc2878d49d9c100ae3582bd146be5b691d
ZSedillo <sedillozandro720@gmail.com>
2025-04-03 22:20:38 +0800

    Adding util

---
+++ server/util/putObject.js
@@ -0,0 +1,25 @@
+const { PutObjectCommand } = require("@aws-sdk/client-s3")
+
+exports.putObject = async(file,fileName) => {
+    try{
+        const params = {
+            Bucket: process.env.AWS_S3_BUCKET,
+            Key: `${fileName}`,
+            Body: file,
+            ContentType: "image/jpg,jpeg,png",
+            // ContentType: "video/mp3,mp4",
+            // ContentType: "application/pdf,docx,xlx",
+        }
+        const command = new PutObjectCommand(params);
+        const data = await s3Client.send(command);
+
+        if(data.$metadata.httpStatusCode != 200){
+            return;
+        }
+        let url = `https://${process.env.AWS_S3_BUCKET}.s3.${process.env.AWS_REGION}.amazonaws.com/${params.Key}`
+        console.log(url);
+        return {url, key:pararms.Key}
+    }catch(err){
+        console.log(err)
+    }
+}
